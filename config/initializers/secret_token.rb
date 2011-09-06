1d4e35af1e50394e1e8dbfa0ed12e4722c107c67
Gehua Huang <bolixie@gmail.com>
2011-09-06 09:56:25 +0800

    initialize project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Site4beidanci::Application.config.secret_token = '44876dfebea56d0d0cd5e4f65b3bc7d4fbacaddf1f75247e455dbc47cfec72cbcab3cd78a84af43d4143ad4363d5f87870d9d995a98b905b1ea4eea33875459d'
